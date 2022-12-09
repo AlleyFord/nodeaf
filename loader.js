40a0b748c1df64f1d1c1375884f3d22bf9929724
Josh Alley <josh@alley.io>
2022-12-09 16:21:31 -0600

    testing

--- loader.js
+++ loader.js
@@ -2,7 +2,8 @@ import CSV from './src/csv.js';
 import PQ from './src/pqueue.js';
 import API from './src/api.js';
 import Cache from './src/cache.js';
+import * as std from './src/std.js';
 
 
 
-export { CSV, PQ, API, Cache };
+export { CSV, PQ, API, Cache, std };
---
+++ src/std.js
@@ -0,0 +1,8 @@
+function die(v) {
+  console.log(v);
+  process.exit();
+}
+
+
+
+export { die };
